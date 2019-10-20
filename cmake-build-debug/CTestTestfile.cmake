4adeaf97652e7b2dfdaaa76d4882686bb1b77669
CrazyOverdose <alenkavh@yandex.ru>
2019-10-21 02:49:03 +0300

    test6

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/absinthetoxin/CrazyOverdose/lab02/lab-02-cache
-# Build directory: /home/absinthetoxin/CrazyOverdose/lab02/lab-02-cache/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/home/absinthetoxin/CrazyOverdose/lab02/lab-02-cache/cmake-build-debug/tests")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/absinthetoxin/CrazyOverdose/lab02/lab-02-cache/CMakeLists.txt;52;add_test;/home/absinthetoxin/CrazyOverdose/lab02/lab-02-cache/CMakeLists.txt;0;")
