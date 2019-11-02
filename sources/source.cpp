0275d08ae0e7fa1bdcbf5935e4e94576b18a24c6
CrazyOverdose <alenkavh@yandex.ru>
2019-11-02 18:26:25 +0300

    test 10

--- sources/source.cpp
+++ sources/source.cpp
@@ -4,148 +4,198 @@
 #include <iostream>
 #include <algorithm>
 #include <cstdlib>
+#include <string>
 #include <ctime>
 #include <vector>
 
 using namespace std;
 
 
-void straight (int mas[], int n)
+void straight(vector <int> mas)
 {
-    for (int i = 0; i < n; i++)
-    {
-        mas[i] = mas[i] + 17;
-    }
+	for (vector<int>::iterator iter = mas.begin(); iter != mas.end(); ++iter)
+	{
+		*iter = *iter + 17;
+	}
+	mas.clear();
 }
-void back (int mas[], int n)
+void back(vector <int> mas)
 {
-    for (int i = n; i > 0; i--)
-    {
-        mas[i] = mas[i] + 17;
-    };
-}
-void random (int mas[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        mas[i] = mas[i] + 17;
-    }
+	for (vector<int>::iterator  iter = mas.end() - 1; iter != mas.begin(); --iter)
+	{
+		*iter = *iter + 17;
+	}
+	mas.clear();
 }
 
-int output1( int mas[], int n)
+void random(vector <int> mas)
 {
-    straight(mas, n);  // прогревание кэша
-    unsigned int start_time = clock();
-    for (int j = 0; j < 1000; j++)
-    {
-        straight(mas, n);
-    }
-    unsigned int end_time = clock();
-    unsigned int search_time = end_time-start_time;
-
-    return search_time;
+	for (vector<int>::iterator  iter = mas.begin(); iter != mas.end(); ++iter)
+	{
+		*iter = *iter + 17;
+	}
+	mas.clear();
 }
-int output2(int mas[], int n)
+
+int output1(vector <int> mas)
 {
-    back(mas, n);  // прогревание кэша
-    unsigned int start_time = clock();
-    for (int j = 0; j < 1000; j++)
-    {
-        back(mas, n);
-    }
-    unsigned int end_time = clock();
-    unsigned int search_time = end_time - start_time;
-    return search_time;
+	straight(mas);  // прогревание кэша
+	unsigned int start_time = clock();
+	for (unsigned int j = 0; j < 1000; ++j)
+	{
+		for (vector<int>::iterator  iter = mas.begin(); iter != mas.end(); ++iter)
+		{
+			*iter = *iter + 17;
+		}
+	}
+	unsigned int end_time = clock();
+	unsigned int search_time = end_time - start_time;
+
+	return search_time;
 }
-int output3(int mas[], int n)
+int output2(vector <int> mas)
 {
-    random(mas, n);  // прогревание кэша
+	back(mas);  // прогревание кэша
+	unsigned int start_time = clock();
+	for (unsigned int j = 0; j < 1000; ++j)
+	{
+		for (vector<int>::iterator iter = mas.end() - 1; iter != mas.begin(); --iter)
+		{
+			*iter = *iter + 17;
+		}
+	}
+	unsigned int end_time = clock();
+	unsigned int search_time = end_time - start_time;
+	return search_time;
+}
+int output3(vector <int> mas)
+{
+	random(mas);  // прогревание кэша
 
-    unsigned int start_time = clock();
+	unsigned int start_time = clock();
 
-    random_shuffle(mas, mas + n);
-    for (int j = 0; j < 1000; j++)
-    {
-        random(mas, n);
-    }
-    unsigned int end_time = clock();
-    unsigned int search_time = end_time - start_time;
-    return search_time;
+	random_shuffle(mas.begin(), mas.end());
+
+	for (unsigned int j = 0; j < 1000; ++j)
+	{
+		for (vector<int>::iterator iter = mas.begin(); iter != mas.end(); ++iter)
+		{
+			*iter = *iter + 17;
+		}
+	}
+	unsigned int end_time = clock();
+	unsigned int search_time = end_time - start_time;
+	return search_time;
+}
+
+class exsperiments
+{
+private:
+	vector <int> random_walk;
+	vector <int> back_walk;
+	vector <int> straight_walk;
+	vector <int> mas;
+	void dimensionts();
+	int number_of_experiments();
+	void exsperiment();
+	void stop();
+public:
+	exsperiments()
+	{
+		dimensionts();
+		exsperiment();
+		stop();
+	}
+};
+
+void exsperiments::dimensionts()
+{
+	int j = number_of_experiments();
+	mas.resize(j);
+	j = 2;
+	for (vector<int>::iterator  iter = mas.begin(); iter != mas.end(); ++iter)
+	{
+		*iter = (j * 128 * 1024) / 4;
+		j = j * 2;
+	}
+
+}
+
+int exsperiments::number_of_experiments()
+{
+	int n = 0;
+	int i = 128;
+	while (i < 9216)
+	{
+		n++;
+		i = i * 2;
+	}
+	return n;
+}
+
+void exsperiments::exsperiment()
+{
+	vector <int> array;
+	for (vector<int>::iterator  iter1 = mas.begin(); iter1 != mas.end(); ++iter1)
+	{
+		array.resize(*iter1);  //установка размера массива
+
+		 //заполнение массива рандомными числами
+		for (vector<int>::iterator  iter = array.begin(); iter != array.end(); ++iter)
+		{
+			*iter = rand() % 20;
+		}
+		straight_walk.push_back(output1(array)); //прямой обход
+		back_walk.push_back(output2(array));   //обратный обход
+		random_walk.push_back(output3(array));  //случайный обход
+
+		array.clear();
+	}
+}
+
+void  exsperiments::stop()
+{
+	unsigned int j = number_of_experiments();
+	vector <string> str = { "straight",  "back",  "random" };
+
+	for (vector<string>::iterator it = str.begin(); it != str.end(); ++it)
+	{
+		cout << "investigation:" << endl;
+		cout << "  travel_variant: " << *it << " " << endl;
+		cout << "  experiments:" << endl;
+		int r1 = 128;
+		for (unsigned int i = 0; i < j; i++)
+		{
+			cout << "  - experiment:" << endl;
+			cout << "      number: " << i + 1 << endl;
+			cout << "      input_data:" << endl;
+			cout << "        buffer_size: '" << r1 << "' kb" << endl;
+			cout << "      results:" << endl;
+			if (*it == "straight")
+			{
+				cout << "        duration: " << straight_walk[i] << " ms" << endl;
+			}
+			if (*it == "back")
+			{
+				cout << "        duration: " << back_walk[i] << " ms" << endl;
+			}
+			if (*it == "random")
+			{
+				cout << "        duration: " << random_walk[i] << " ms" << endl;
+			}
+			r1 = r1 * 2;
+		}
+
+
+	}
 }
 
 int main()
 {  // 1 int = 4 байт, 1kb = 1024 b
-    int mas[7];   //массив размерности в байтах
-    int r1 = 128; // 128 kb = 131072 b (1/2 от первого уровня)
-    //int n = 2359296; int r5 = 9216; // 9216 kb = 9437184 b (3/2 третьего уровня)
+	//int n1 = 32768; int r1 = 128; // 128 kb = 131072 b (1/2 от первого уровня)
+	//int n = 2359296; int r5 = 9216; // 9216 kb = 9437184 b (3/2 третьего уровня)
 
-    vector <int> random_walk;
-    vector <int> back_walk;
-    vector <int> straight_walk;
-
-    for (int i = 1; i < 8; i++)
-    {
-        mas[i - 1] = (r1 * i * 1024) / 4;
-    }
-
-    srand(time(0));
-
-    for (int j = 0; j < 7; j++)
-    {
-        int *array = new int[mas[j]];  //создание массива
-
-      for (int i = 0; i < mas[j]; i++)
-      {
-        array[i] = rand() % 20;
-      }
-        straight_walk.push_back(output1(array, mas[j])); //прямой обход
-        back_walk.push_back(output2(array, mas[j]));   //обратный обход
-        random_walk.push_back(output3(array, mas[j]));  //случайный обход
-
-    }
-
-    cout << "investigation:" << endl;
-    cout << "  travel_variant: straight " << endl;
-    cout << "  experiments:" << endl;
-    for (int i = 0; i < 7; i++)
-    {
-        cout << "  - experiment:" << endl;
-        cout << "      number: " << i+1 << endl;
-        cout << "      input_data:" << endl;
-        cout << "        buffer_size: '" << r1 << "' kb" << endl;
-        cout << "      results:" << endl;
-        cout << "        duration: " << straight_walk[i] << " ms" << endl;
-        r1 = r1 * 2;
-    }
-    cout << endl;
-    cout << "investigation:" << endl;
-    cout << "  travel_variant: back " << endl;
-    cout << "  experiments:" << endl;
-    r1 = 128;
-    for (int i = 0; i < 7; i++)
-    {
-        cout << "  - experiment:" << endl;
-        cout << "      number: " << i + 1 << endl;
-        cout << "      input_data:" << endl;
-        cout << "        buffer_size: '" << r1 << "' kb" << endl;
-        cout << "      results:" << endl;
-        cout << "        duration: " << back_walk[i] << " ms" << endl;
-        r1 = r1 * 2;
-    }
-    cout << endl;
-    cout << "investigation:" << endl;
-    cout << "  travel_variant: random " << endl;
-    cout << "  experiments:" << endl;
-    r1 = 128;
-    for (int i = 0; i < 7; i++)
-    {
-        cout << "  - experiment:" << endl;
-        cout << "      number: " << i + 1 << endl;
-        cout << "      input_data:" << endl;
-        cout << "        buffer_size: '" << r1 << "' kb" << endl;
-        cout << "      results:" << endl;
-        cout << "        duration: " << random_walk[i] << " ms" << endl;
-        r1 = r1 * 2;
-    }
-    return 0;
+	srand(time(0));
+	exsperiments Te;
+	return 0;
 }
